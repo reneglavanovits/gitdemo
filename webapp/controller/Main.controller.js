37873af3d15247269aec6446c78a503b12cf68cf
Rene Glavanovits <rg@clouddna.at>
2022-04-14 20:57:58 +0000

    Initial Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("at.clouddna.GitDemo.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"at/clouddna/GitDemo/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
